50df31b4e2e2abe9e41de1eac36bdf5f645821b9
joshcarp <joshuacarpeggiani@gmail.com>
2021-10-16 18:40:03 +1100

    Introduct distance, distancex and distancexx

--- Proj2.hs
+++ Proj2.hs
@@ -1,6 +1,7 @@
 module Proj2 (Location, toLocation, fromLocation, feedback, GameState, initialGuess, nextGuess) where
 import Data.Char
 import Text.Printf
+import qualified Data.Set as Set
 
 type Location  = (Int, Int)
 type GameState = Int
@@ -13,7 +14,13 @@ fromLocation :: Location -> String
 fromLocation (x, y) = (printf "%c%d" (toUpper (chr (ord 'a' + x - 1))) y)
 
 feedback :: [Location] -> [Location] -> (Int,Int,Int)
-feedback _ _ = (0, 0, 0)
+feedback x y = (0, 0, 0) -- ((distancexx x y 0), (distancexx x y 1), (distancexx x y 2))
+
+distancexx :: (RealFrac a1, Integral a2, Floating a1) => [(a1, a1)] -> [(a1, a1)] -> a2 -> Int
+distancexx a b z = sum [distancex a c z | c <- b]
+
+distancex :: (RealFrac a1, Integral a2, Floating a1) => [(a1, a1)] -> (a1, a1) -> a2 -> Int
+distancex a b n = length [c | c <- a, (distance c b) == n ]
 
 distance :: (RealFrac a, Integral b, Floating a) => (a, a) -> (a, a) -> b
 distance (x1, y1) (x2, y2) = floor( sqrt ((x1 - x2)^2 + (y1 - y2)^2))
