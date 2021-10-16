6b678ba88ba8ead5f7ff9f13d8827c3e96ba3ba9
joshcarp <joshuacarpeggiani@gmail.com>
2021-10-16 22:44:09 +1100

    Broken

--- Proj2_testing.hs
+++ Proj2_testing.hs
@@ -3,11 +3,20 @@ import Test.QuickCheck
 import Text.Printf
 import Data.Char
 import Proj2
+import Test.Hspec
+import Test.QuickCheck
 
 checkLocation ::  Char -> Int -> Property
 checkLocation x y =   elem x ['A' .. 'H']
          ==> ((toLocation (printf "%c%d" x y)) >>= (\r -> return (fromLocation r)))
          === Just (printf "%c%d" x y)
 
+prop_abs :: Int -> Int
+prop_abs n = n
+
 main :: IO ()
-main = quickCheck (checkLocation)
+main = hspec $ do
+  describe "Prelude.read" $ do
+    it "can parse integers" $ do
+      (toLocation "a2") `shouldBe` (1, 2)
+
