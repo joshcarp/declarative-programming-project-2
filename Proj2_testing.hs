d466a132d15a081e0488a2b08e3b978c9ac32054
joshcarp <joshuacarpeggiani@gmail.com>
2021-10-16 14:18:30 +1100

    Format CheckLocation

--- Proj2_testing.hs
+++ Proj2_testing.hs
@@ -4,13 +4,10 @@ import Text.Printf
 import Data.Char
 import Proj2
 
-prop ::  Char -> Int -> Property
-prop x y =  elem x ['A' .. 'H']
-            ==>
-            ((toLocation (printf "%c%d" x y)) >>= (\r -> return (fromLocation r)))
-            ===
-            Just (printf "%c%d" x y)
-
+checkLocation ::  Char -> Int -> Property
+checkLocation x y =   elem x ['A' .. 'H']
+         ==> ((toLocation (printf "%c%d" x y)) >>= (\r -> return (fromLocation r)))
+         === Just (printf "%c%d" x y)
 
 main :: IO ()
-main = quickCheck (prop)
+main = quickCheck (checkLocation)
